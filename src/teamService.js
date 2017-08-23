1635052cd9a732ad57a3a4e0a7b4bab628e615df
Taylor McKay <taylormckay68@gmail.com>
2017-08-22 22:34:06 -0600

    basic template for review

---
+++ src/teamService.js
@@ -0,0 +1,124 @@
+const teamA = [
+    {
+        number: 11,
+        first_name: "Alec",
+        last_name: "Jackson"
+    },
+    {
+        number: 21,
+        first_name: "Alex",
+        last_name:  "Ellsworth"
+    },
+    {
+        number: 32,
+        first_name: "Anthony",
+        last_name:  "Caccavella"
+    },
+    {
+        number: 48,
+        first_name: "Anthony",
+        last_name: "Montoya"
+    },
+    {
+        number: 55,
+        first_name: "Ben",
+        last_name: "Ahlander"
+    },
+    {
+        number: 63,
+        first_name: "Chandler",
+        last_name: "Stolworthy"
+    },
+    {
+        number: 71,
+        first_name: "Daniel",
+        last_name: "Valentine"
+    },
+    {
+        number: 88,
+        first_name: "James",
+        last_name: "Carlson"
+    },
+    {
+        number: 94,
+        first_name: "Juan",
+        last_name: "Jacinto"
+    },
+    {
+        number: 13,
+        first_name: "Kegan",
+        last_name: "Swensen"
+    },
+    {
+        number: 16,
+        first_name: "Keith",
+        last_name: "Dnumberier"
+    }
+
+]
+
+const teamB = [
+    {
+        number: 16,
+        first_name: "Kim",
+        last_name: "Scarabello"
+    },
+    {
+        number: 24,
+        first_name: "Leah",
+        last_name:  "Hills"
+    },
+    {
+        number: 38,
+        first_name: "Marissa",
+        last_name:  "Fischbeck"
+    },
+    {
+        number: 42,
+        first_name: "Maxwell",
+        last_name: "Rasmussen"
+    },
+    {
+        number: 54,
+        first_name: "Paul",
+        last_name: "Polzer"
+    },
+    {
+        number: 69,
+        first_name: "Sam",
+        last_name: "Montoya"
+    },
+    {
+        number: 70,
+        first_name: "Seth",
+        last_name: "Stephenson"
+    },
+    {
+        number: 85,
+        first_name: "Shawn",
+        last_name: "Koga"
+    },
+    {
+        number: 94,
+        first_name: "Trent",
+        last_name: "Bennett"
+    },
+    {
+        number: 19,
+        first_name: "Vincent",
+        last_name: "Palmer"
+    },
+    {
+        number: 1,
+        first_name: "Blake",
+        last_name: "Adams"
+    }
+]
+
+export function getTeamA() {
+    return teamA;
+}
+
+export function getTeamB() {
+    return teamB;
+}
